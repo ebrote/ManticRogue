e646d18c1a089d98068f02e4ca1651b8b4df5ac8
ebrote <erik.brote@gmail.com>
2017-11-14 10:23:03 +0100

    Clustering
    First functioning version of clustering algorithm

--- analysis.py
+++ analysis.py
@@ -1,25 +1,32 @@
 
-# import sqlalchemy
-# import csv
-# import requests
-# from bs4 import BeautifulSoup
+import sqlalchemy
+import csv
+import requests
+from bs4 import BeautifulSoup
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Date
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import MetaData, Table
 from sqlalchemy.orm import mapper
-# import re
-# import unidecode
-# from fuzzywuzzy import fuzz
-# from fuzzywuzzy import process
-# import matplotlib.pyplot as plt
+import re
+import unidecode
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import matplotlib.pyplot as plt
 
 import numpy as np
 import pandas as pd
+import datetime as dt
+
+from sklearn import metrics
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import scale
+from time import time
 
 import math
 
+####### Database extraction ######
 class Mairies():
 	pass
 
@@ -41,42 +48,43 @@ def data_frame(query, columns):
 # dataframe with all fields in the table
 query = session.query(Mairies).all()
 DF = data_frame(query, ["insee_code","postal_code","city", "population" ,"latitude" ,"longitude","first_name","last_name","birthdate","first_mandate_date","party"])
-print(DF.shape)
+
+DF2 = pd.DataFrame(session.query(mairies).all()).query("latitude != 'None' and longitude != 'None'")
 DF["population"] = DF["population"].apply(pd.to_numeric)
 #DF["first_mandate_date"] = DF["first_mandate_date"].apply(pd.to_numeric)
 # DF['latitude'] = DF['latitude'].astype(numeric)
 
 
-# def import_data():
-# 	'''
-# 	TODO : 
-# 	Code that will eventually import our scrapped data
-# 	'''
-# 	query = session.query(Mairies).all()
-
-# 	df = pd.read_sql(query,engine)
-
-# 	# df = pd.concat([df1,df2],axis=1)
-# 	return df
-
-DF_filtered = DF[DF.population>5000]
-
-#####################################
-from sklearn import metrics
-from sklearn.cluster import KMeans
-from sklearn.preprocessing import scale
-from time import time
+####### Data analysis ######
 
 
-n_clusters=5
-DF2 = DF_filtered[['latitude','longitude']]#,'birthdate','first_mandate_date']]
-DF2 = DF2[DF2.latitude != 'None']
-DF2 = DF2[DF2.longitude != 'None']
-data = DF2.as_matrix()
+DF['birthyear'] = DF['birthdate'].apply(lambda x: (dt.datetime.today() - dt.datetime.strptime(x.strip(), '%Y-%m-%d')).days//365)
+
+def data_clense(DF):
+	data_scale = scale(DF)
+	return data_scale
+
+def clustering(DF_o,n_clusters=5):
+	data = DF_o.as_matrix()
 
 
-kmeans = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
-kmeans.fit(data)
+	kmeans = KMeans(init='k-means++', n_clusters=n_clusters, n_init=10)
+	kmeans.fit(data)
 
-Z = kmeans.predict(data)
-print(Z)
+	Z = kmeans.predict(data)
+
+	DF_cluster = pd.DataFrame(Z,columns=['cluster'])
+	df = pd.concat([DF2, DF_cluster], axis=1)
+	print(df.head())
+
+	return DF
+
+DF_filtered = DF2[DF2.population>5000]
+
+DF3 = DF_filtered[['latitude','longitude']]#,'birthyear']]
+#DF2 = DF2[DF2.latitude != 'None']
+# DF2 = DF2[DF2.longitude != 'None']
+print(DF3.head())
+
+DF3 = clustering(DF3)
+print(DF3.head())
